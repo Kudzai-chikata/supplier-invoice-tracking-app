6bae87c86a347d9f2c630d7c2549948cbacb2ea1
Kudzai-chikata <kupachikata17@gmail.com>
2026-03-24 11:50:28 +0200

    Add Riverpod provider for network connectivity status

---
+++ lib/presentation/providers/connectivity_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+final connectivityProvider = StreamProvider<ConnectivityResult>((ref) {
+  final Connectivity connectivity = Connectivity();
+  return connectivity.onConnectivityChanged;
+});
